a061a4056957af2d356c141d36b9ee3c322dfbd5
Sulaiman <suluc4u@gmail.com>
2021-04-19 09:29:31 +0530

    sqlite update

--- Travel_Portal/settings.py
+++ Travel_Portal/settings.py
@@ -78,19 +78,14 @@ WSGI_APPLICATION = 'Travel_Portal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'travelport',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 db_from_env = dj_database_url.config(conn_max_age=600)
