b201f3154c4dcc7c442aa2b2cc8749b6d8eb9fdf
Sulaiman <suluc4u@gmail.com>
2021-04-24 13:42:34 +0530

    login page connected to the project

--- login/views.py
+++ login/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate,login
 
 # Create your views here.
 def login(request):
+    if request.method=='POST':
+
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            return redirect('/dashboard/')
+        else:
+            print("not a valid user")
+
+
 
     return render(request,'login.html')
